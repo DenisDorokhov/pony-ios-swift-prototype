098e2f8d40ca17f6f9c8faa9980307a132b40244
DenisDorokhov <denis@dorokhov.net>
2016-04-26 19:54:16 +0200

    splash screen, project structure

---
+++ source/Pony/Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pony
+//
+//  Created by Denis Dorokhov on 26/04/16.
+//  Copyright © 2016 Denis Dorokhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
